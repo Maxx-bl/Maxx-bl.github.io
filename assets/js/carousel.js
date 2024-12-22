d5120bf96223d8fb08c1ce78bf58c8a3739a376f
Maxx <max13.bl@gmail.com>
2024-12-22 19:40:14 +0100

    text indicator work page

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -2,12 +2,20 @@ const track = document.querySelector('.carousel-track');
 const slides = Array.from(track.children);
 const nextButton = document.querySelector('.carousel-button.next');
 const prevButton = document.querySelector('.carousel-button.prev');
+const indicatorText = document.querySelector('#index-indicator');
 
 let currentSlideIndex = 0;
+
+window.onload = () => updateTextIndicator();
 
 function updateSlidePosition() {
     const slideWidth = slides[0].getBoundingClientRect().width;
     track.style.transform = `translateX(-${currentSlideIndex * slideWidth}px)`;
+    updateTextIndicator();
+}
+
+function updateTextIndicator() {
+    indicatorText.textContent = `${currentSlideIndex + 1}/${slides.length}`;
 }
 
 nextButton.addEventListener('click', () => {
